cfe41c940502e95ad6da0a5898c7574086f07431
Marcilia <guilgerm@gmail.com>
2022-03-29 07:24:10 -0300

    add prometheus

---
+++ src/DogBank.Api/Application/Configurations/PrometheusConfigurations.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Prometheus;
+
+namespace DogBank.Api.Application.Configurations
+{
+    public static class PrometheusConfigurations
+    {
+        public static void ConfigurePrometheus(this IApplicationBuilder app)
+        {
+            var counter = Metrics
+                .CreateCounter("DogBankApi", "Counts requests to the API",
+                    new CounterConfiguration
+                    {
+                        LabelNames = new[] { "method", "endpoint" }
+                    });
+
+
+            app.Use((context, next) =>
+            {
+                counter.WithLabels(context.Request.Method, context.Request.Path).Inc();
+                return next();
+            });
+
+            app.UseMetricServer();
+            app.UseHttpMetrics();
+        }
+    }
+}
--- src/DogBank.Api/Startup.cs
+++ src/DogBank.Api/Startup.cs
@@ -1,15 +1,10 @@
+using DogBank.Api.Application.Configurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DogBank.Api
 {
@@ -42,6 +37,8 @@ namespace DogBank.Api
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DogBank.Api v1"));
             }
+
+            app.ConfigurePrometheus();
 
             app.UseRouting();
 
